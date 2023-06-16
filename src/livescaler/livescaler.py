36d3b979b9422bad80e367fda4950b298e17a062
Alice Rixte <alice.rixte@gmail.com>
2023-06-16 15:21:14 +0200

    Periodic transformations

--- src/livescaler/livescaler.py
+++ src/livescaler/livescaler.py
@@ -1,35 +1,40 @@
 from abc import ABC, abstractmethod
-
-from enum import Enum
-
+import numpy as np
 
 class Transform(ABC):
     def __init__(self, anchor = 0, base = 12) : 
         self.anchor = anchor
         self.base = base
+
     @abstractmethod
-    def eval(self, arg) : 
+    def eval_diff(self,arg) : 
         pass
+
+    def eval(self, arg) : 
+        return arg + self.eval_diff(arg)
 
     @abstractmethod
     def __rshift__(self, other) : 
         pass
 
+    def __str__(self) : 
+        return f"[anchor : {self.anchor}, base : {self.base}]"
+
 class Affine(Transform): 
     def __init__(self, mult, transp, anchor = 0, base = 12)  : 
         """! The Affine initializer
         @param mult The multiplicator coefficient of the affine transform
-        @param transp The transposition paraleter of the affine transform
+        @param transp The transposition parameter of the affine transform
         @return  An affine transformation.
         """
         super().__init__(anchor, base)
         self.mult = mult
         self.transp = transp
 
-    def eval(self,arg) : 
-        return arg * self.mult + self.transp
+    def eval_diff(self,arg) : 
+        return arg * (self.mult - 1) + self.transp
     
-    def __lshift__(self,other) : 
+    def __rshift__(self,other) : 
         newmult = self.mult * other.mult
         newtransp = other.mult * self.transp + other.transp
         return Affine(newmult, newtransp)
@@ -42,7 +47,9 @@ class Affine(Transform):
         newtransp = other.transp + self.transp
         return Affine(newmult, newtransp)
     
-    
+    def __str__(self) : 
+        return f"[mult : {self.mult}, transp : {self.transp}, anchor : {self.anchor}, base : {self.base}]"
+
 class StdAffine : 
     I = Affine(1,0)
     i = Affine(-1,-5)
@@ -58,3 +65,24 @@ class StdAffine :
     vi = Affine(-1,4)
     VII = Affine(1,11)
     vii = Affine(-1,6)
+
+
+class Periodic(Transform) : 
+    def __init__(self, period, anchor = 0) : 
+        super().__init__(anchor, len(period))
+        self.period = period
+
+    def eval_diff(self, arg) : 
+        return self.period[(arg + self.anchor) % self.base]
+    
+    def __rshift__(self, other):
+        if(self.base != other.base) :
+            raise ValueError("Can't compose two transformations with different base")
+        anchorshift =  other.anchor - self.anchor
+        newperiod = np.roll(self.period, anchorshift)  +  other.period
+        return Periodic(newperiod,other.anchor)
+    
+    def __str__(self) : 
+        return f"[period : {self.period}, anchor : {self.anchor}, base : {self.base}]"
+    
+
--- tests/test_livescaler.py
+++ tests/test_livescaler.py
@@ -3,6 +3,7 @@
 # setup.py that excludes installing the "tests" package
 
 import unittest
+import numpy as np
 
 import livescaler.livescaler as ls
 
@@ -11,7 +12,18 @@ class TestLivescaler(unittest.TestCase):
 
     def test_affine(self) : 
         aff = ls.StdAffine.iii
-        self.assertEqual(aff.eval(4), -5)
+        assert aff.eval(4) == -5
+    
+    def test_periodic(self) :
+        diff4 = ls.Periodic(np.array([0,1,2,3]),1)
+        inv4 = ls.Periodic(np.array([3,2,1,0]),3)
+        comp = diff4 >> inv4
+        assert comp.anchor == 3
+        assert all(comp.period == np.array([5,5,1,1]))
+        rescomp = comp.eval(np.array([0,1,2,3]))
+        assert all(rescomp == np.array([1,6,7,4]))
+        
+        #self.assertEqual(rescomp, np.array([1,6,7,4]))
 
 
 if __name__ == '__main__':
